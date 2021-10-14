3cb4c93c51ab8a61dd2ac0d35012d2de30adf7a8
FernandoLuc4s <fernandolucas2002@gmail.com>
2021-10-14 19:44:03 -0300

    O Exercicio da aula 7 sobre fila foi resolvido, neste esercicio tivemos que inserir nomes em uma fila e logo após mostra-los

--- Aula 7 - Fila/src/filas/Exerc.java
+++ Aula 7 - Fila/src/filas/Exerc.java
@@ -1,10 +1,28 @@
 package filas;
+import java.util.Scanner;
 
 public class Exerc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan =  new Scanner(System.in);
+		
+		FilaExerc fila = new FilaExerc();
+		
+		String nome;
+		
+		System.out.println("Digite os nomes da fila: ");
+		for (int i = 0; i < 5; i++) {
+			nome = scan.nextLine();
+			fila.inserir(nome);
+		}
+		System.out.println();
+		while (!fila.vazia()) {
+			nome = fila.retirar();
+			System.out.println(nome +" ");
+			
+		}
+		
 	}
 
 }
--- Aula 7 - Fila/src/filas/FilaExerc.java
+++ Aula 7 - Fila/src/filas/FilaExerc.java
@@ -1,5 +1,44 @@
 package filas;
 
 public class FilaExerc {
-
+	public String[] nomes;
+	public int inicio;
+	public int fim;
+	public int total;
+	
+	public FilaExerc() { // metotodo construto
+		nomes = new String[5]; // tamanho do vetor
+		 inicio = fim = total =0; // inicializando as tr~es variaveis
+	}
+	
+	public void inserir(String nome) 
+	{
+		nomes[fim] = nome;
+		fim++;
+		
+		if(fim ==nomes.length) 
+		{
+			fim=0;
+		}
+		total++;
+	}
+	public String retirar() {
+		String nome = nomes[inicio];
+		inicio++;
+		total--;
+		return nome;
+	}
+	public void mostrar() {
+		for (int i = inicio; i < 5; i++) {
+			System.out.println(nomes[i]+" ");
+		}
+		System.out.println();
+	}
+	public boolean vazia() {
+		return total==0;
+	}
+	public boolean cheia() {
+		return total==nomes.length;
+	}
+	
 }
